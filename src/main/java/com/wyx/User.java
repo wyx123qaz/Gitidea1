542b6d3ce6615a53adb9146be450160d43db64c7
wyx <3119064605@qq.com>
2019-11-23 20:44:00 +0800

    提交

--- src/main/java/com/wyx/User.java
+++ src/main/java/com/wyx/User.java
@@ -7,4 +7,7 @@ public class User {
     public static void main(String[] args) {
         System.out.println("你好，武永鑫！！！");
     }
+    public void main1() {
+        System.out.println("你好，武永鑫！！！");
+    }
 }
